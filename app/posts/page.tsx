af54f82730a1556a4e74d7e39f6ceef5d4bde252
Yusi <yusi4781@naver.com>
2025-06-04 11:34:10 +0900

    feat: post button mode 구현 중

---
+++ app/posts/page.tsx
@@ -0,0 +1,9 @@
+import PostButtonList from "@/components/PostButtonList";
+
+export default function PostListPage() {
+  return (
+    <div className="p-6">
+      <PostButtonList />
+    </div>
+  )
+}
