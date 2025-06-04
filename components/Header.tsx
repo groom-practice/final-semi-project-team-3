8e0944347c5871ad7ef0b458a4968919267208c0
Yusi <yusi4781@naver.com>
2025-06-04 14:21:47 +0900

    style: header z-index 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="fixed top-0 left-0 w-screen flex justify-between bg-indigo-800 px-8 py-4">
+    <header className="fixed top-0 left-0 w-screen flex justify-between bg-indigo-800 px-8 py-4 z-1000">
       <nav className="flex gap-5">
         <Link
           href={"/"}
