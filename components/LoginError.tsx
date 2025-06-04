db71414c26ed1ea9c9bd1b9ce37a87659b6a23be
ydsic <ydsic99@gmail.com>
2025-06-04 11:33:34 +0900

    feat: login 기능 구현

---
+++ components/LoginError.tsx
@@ -0,0 +1,7 @@
+export default function LoginError() {
+  return (
+    <div className="bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded-md mt-4">
+      아이디 또는 비밀번호가 올바르지 않습니다.
+    </div>
+  );
+}
