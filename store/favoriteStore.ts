d0ce5029ecbbbe2706837da328894eb2e3f0188c
syrnjs <ksy21019@gmail.com>
2025-06-05 09:38:55 +0900

    feat: 새로 고침 시 즐겨찾기 목록 유지 기능 추가

--- store/favoriteStore.ts
+++ store/favoriteStore.ts
@@ -1,16 +1,25 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type FavoriteStore = {
   favorites: number[];
   toggleFavorite: (id: number) => void;
 };
 
-export const useFavoriteStore = create<FavoriteStore>((set) => ({
-  favorites: [],
-  toggleFavorite: (id) =>
-    set((state) => ({
-      favorites: state.favorites.includes(id)
-        ? state.favorites.filter((fav) => fav !== id)
-        : [...state.favorites, id],
-    })),
-}));
+export const useFavoriteStore = create<FavoriteStore>()(
+  persist(
+    (set, get) => ({
+      favorites: [],
+      toggleFavorite: (id: number) =>
+        set((state) => ({
+          favorites: state.favorites.includes(id)
+            ? state.favorites.filter((fav) => fav !== id)
+            : [...state.favorites, id],
+        })),
+    }),
+    {
+      name: "favorite-posts",
+      partialize: (state) => ({ favorites: state.favorites }),
+    }
+  )
+);
