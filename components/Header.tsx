bd1214ffe1454329d709e1841521a57719801646
ydsic <ydsic99@gmail.com>
2025-06-05 09:49:52 +0900

    feat: 로그인 후 이용가능하게 로직 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,8 @@ export default function Header() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
   const logout = useAuthStore((state) => state.logout);
   const initLogin = useAuthStore((state) => state.initLogin);
+  const [showModal, setShowModal] = useState(false);
+
   const router = useRouter();
 
   const handleLogout = () => {
@@ -23,6 +25,18 @@ export default function Header() {
     }
   }, []);
 
+  const handleProtectedRoute = (e: React.MouseEvent, path: string) => {
+    if (!isLoggedIn) {
+      e.preventDefault();
+      setShowModal(true);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 2000);
+    } else {
+      router.push(path);
+    }
+  };
+
   return (
     <header className="fixed top-0 left-0 w-screen flex justify-between bg-indigo-800 px-8 py-4 z-1000">
       <nav className="flex gap-5">
@@ -32,18 +46,20 @@ export default function Header() {
         >
           Home
         </Link>
-        <Link
-          href={"/photos"}
-          className="text-white hover:scale-105 hover:text-blue-200"
+        <a
+          href="/photos"
+          onClick={(e) => handleProtectedRoute(e, "/photos")}
+          className="text-white hover:scale-105 hover:text-blue-200 cursor-pointer"
         >
           Photos
-        </Link>
-        <Link
-          href={"/posts"}
-          className="text-white hover:scale-105 hover:text-blue-200"
+        </a>
+        <a
+          href="/posts"
+          onClick={(e) => handleProtectedRoute(e, "/posts")}
+          className="text-white hover:scale-105 hover:text-blue-200 cursor-pointer"
         >
           Posts
-        </Link>
+        </a>
       </nav>
       <nav>
         {isLoggedIn ? (
@@ -62,6 +78,12 @@ export default function Header() {
           </Link>
         )}
       </nav>
+
+      {showModal && (
+        <div className="fixed bottom-30 left-1/2 transform -translate-x-1/2 bg-red-500 text-white text-lg font-medium px-8 py-3 rounded shadow-lg transition-opacity duration-300 z-[9999]">
+          로그인이 필요합니다.
+        </div>
+      )}
     </header>
   );
 }
